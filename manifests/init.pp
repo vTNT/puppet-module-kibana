bb208a9b9f00acc35be585903cecfb64edfbd116
root <root@server.puppet.com>
2014-04-15 15:35:12 +0800

    add kibana module

---
+++ manifests/init.pp
@@ -0,0 +1,58 @@
+class kibana (
+    $home_path = "/usr/local/nginx/conf/vhost",
+    $vhost_conf = "/usr/local/nginx/conf/vhost/kibana.conf",
+    $vhost = "kibana.xx.com",
+) {
+
+    file {$home_path:
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
+
+    file {"kibana_vhost":
+       ensure  => present,
+       owner   => 'root',
+       group   => 'root',
+       mode    => '0644',
+       require => file[$home_path],
+       path    => $vhost_conf,
+       content => template('kibana/vhost.erb'),
+       notify  => Service["nginxd"],
+    }
+
+   file {"kibana":
+        ensure => present,
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+        path => '/tmp/kibana-3.0.1-1.x86_64.rpm',
+        source => 'puppet:///modules/kibana/kibana-3.0.1-1.x86_64.rpm', 
+        require => File["kibana_vhost"],
+    }
+
+    package {"kibana":
+        ensure  => present,
+        provider => rpm,
+        source   => "/tmp/kibana-3.0.1-1.x86_64.rpm",
+        require => File["kibana"],
+    } 
+
+    file {"config.js":
+        ensure => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package["kibana"],
+        path    => '/var/www/html/kibana/config.js',
+        content => template('kibana/config.erb'),
+    }
+
+    service { "nginxd":
+        ensure  => running,
+        enable  => true,
+        subscribe => File['kibana_vhost'],
+    }
+
+}
